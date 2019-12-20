211fdff129ce7bed6fd00f3f8004bfe22c810922
degoyto <degoyto@gmail.com>
2019-12-20 10:08:01 -0300

    aaaa

--- server/src/app.js
+++ server/src/app.js
@@ -13,8 +13,10 @@ const morgan = require("morgan")
 
 const app = express();
 app.use (morgan('combined'))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(favicon(__dirname+'/public/favicon.ico'))
 app.use(cors())
 
 
